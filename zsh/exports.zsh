ea2a3c97858a6d7f34909a05d6806251d67f0c10
butlerx <cianbutlerx@gmail.com>
2016-12-17 22:43:12 +0000

    For real, this time.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -10,7 +10,6 @@ LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:c
 export LS_COLORS
 
 # Enable color in grep
-export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='3;33'
 
 # This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -183,6 +183,339 @@ send() {
   return 0
 }
 
-docker-ip() {
+docker-ip () {
   docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}, {{end}}' "$@"
+}
+
+up () {
+  LEVEL=$1
+  for ((i = 1; i <= LEVEL; i++))
+  do
+    CDIR=../$CDIR
+  done
+  cd $CDIR
+  echo "You are in: "$PWD
+}
+
+add () {
+  a=$1; b=$2
+  echo "$a + $b = $(expr $a + $b)"
+}
+
+sub () {
+  a=$1; b=$2
+  echo "$a - $b = $(expr $a - $b)"
+}
+
+mulp () {
+  a=$1; b=$2
+  echo "$a * $b = $(expr $a \* $b)"
+}
+
+div () {
+  a=$1; b=$2
+  echo "$a / $b = $(expr $a / $b)"
+}
+
+armstrong () {
+  n=$1
+  arm=0
+  temp=$n
+  while [ $n -ne 0 ]
+  do
+    r=$(expr $n % 10)
+    arm=$(expr $arm + $r \* $r \* $r)
+    n=$(expr $n / 10)
+  done
+  if [ $arm -eq $temp ]
+  then
+    echo $temp "is Armstrong"
+  else
+    echo $temp "Not Armstrong"
+  fi
+}
+
+Binary2Decimal () {
+  Binary=$1;
+  re='^[0-9]+$';
+  if ! [[ $Binary =~ $re ]]; then
+    echo "Enter a valid number "
+  else
+    while [ $Binary -ne 0 ]; do
+      Bnumber=$Binary
+      Decimal=0
+      power=1
+      while [ $Binary -ne 0 ]; do
+        rem=$(expr $Binary % 10 )
+        Decimal=$((Decimal+(rem*power)))
+        power=$((power*2))
+        Binary=$(expr $Binary / 10)
+      done
+      echo  "$Decimal"
+    done
+  fi
+}
+
+Decimal2Binary () {
+  for ((i=32;i>=0;i--)); do
+    r=$(( 2**$i))
+    Probablity+=( $r  )
+  done
+  [[ $# -eq 0 ]] && { echo -e "Usage \n$0 numbers"; }
+  for input_int in $@; do
+    echo -en "Decimal\t\tBinary\n"
+    s=0
+    test ${#input_int} -gt 11 && { echo "Support Upto 10 Digit number :: skiping \"$input_int\""; continue; }
+    printf "%-10s\t" "$input_int"
+    for n in ${Probablity[@]}; do
+      if [[ $input_int -lt ${n} ]]; then
+        [[ $s = 1 ]] && printf "%d" 0
+      else
+        printf "%d" 1 ; s=1
+        input_int=$(( $input_int - ${n} ))
+      fi
+    done
+  echo -e
+  done
+}
+
+isprime () {
+  n=$1; i=1; c=1;
+  while [ $i -le $n ]; do
+    i=$(expr $i + 1)
+    r=$(expr $n % $i)
+    if [ $r -eq 0 ]; then
+      c=$(expr $c + 1)
+    fi
+  done
+  if [ $c -eq 2 ]; then
+    echo Prime
+  else
+    echo Not Prime
+  fi
+}
+
+colour () {
+  spam=$1
+  clear
+  echo -e "\033[1m $spam"
+   # bold effect
+  echo -e "\033[5m Blink"
+         # blink effect
+  echo -e "\033[0m $spam"
+   # back to noraml
+  echo -e "\033[31m $spam"
+   # Red color
+  echo -e "\033[32m $spam"
+   # Green color
+  echo -e "\033[33m $spam"
+   # See remaing on screen
+  echo -e "\033[34m $spam"
+  echo -e "\033[35m $spam"
+  echo -e "\033[36m $spam"
+  echo -e -n "\033[0m"
+    # back to noraml
+  echo -e "\033[41m $spam"
+  echo -e "\033[42m $spam"
+  echo -e "\033[43m $spam"
+  echo -e "\033[44m $spam"
+  echo -e "\033[45m $spam"
+  echo -e "\033[46m $spam"
+  echo -e "\033[0m $spam"
+}
+
+lowercase () {
+  fileName=$1
+  if [ ! -f $fileName ]; then
+    echo "Filename $fileName does not exists"
+    exit 1
+  fi
+  tr '[A-Z]' '[a-z]' < $fileName >> small.txt
+}
+
+diskUsed () {
+  PART=nvme0n1p3
+  USE=`df -h |grep $PART | awk '{ print $5 }' | cut -d'%' -f1`
+  echo "Percent used: $USE"
+}
+
+process () {
+  echo "Hello $USER"
+  echo "Hey i am" $HOST "and will be telling you about the current processes"
+  echo "Running processes List"
+  ps
+}
+
+dual-screen () {
+  xrandr --output eDP1 --auto --output DP1 --auto --left-of eDP1
+  nohup startDualBar </dev/null >/dev/null 2>&1 &
+}
+
+startDualBar () {
+  killall -q polybar
+  # Wait until the processes have been shut down
+  while pgrep -x polybar >/dev/null; do sleep 1; done
+  nohup polybar i3 </dev/null >/dev/null 2>&1 &
+  nohup polybar second </dev/null >/dev/null 2>&1 &
+}
+
+single-screen () {
+  xrandr --auto
+  nohup startbar </dev/null >/dev/null 2>&1 &
+}
+
+startbar () {
+  killall -q polybar
+  # Wait until the processes have been shut down
+  while pgrep -x polybar >/dev/null; do sleep 1; done
+  nohup polybar i3 </dev/null >/dev/null 2>&1 &
+}
+
+specialPattern () {
+  MAX_NO=$1
+  if ! [ $MAX_NO -ge 5 -a $MAX_NO -le 9 ] ; then
+    echo "WTF... I ask to enter number between 5 and 9, Try Again"
+  else
+    clear
+    for (( i=1; i<=MAX_NO; i++ )); do
+      for (( s=MAX_NO; s>=i; s-- )); do
+        echo -n " "
+      done
+      for (( j=1; j<=i;  j++ )); do
+        echo -n " ."
+      done
+      echo ""
+    done
+    for (( i=MAX_NO; i>=1; i-- )); do
+      for (( s=i; s<=MAX_NO; s++ )); do
+        echo -n " "
+      done
+      for (( j=1; j<=i;  j++ )); do
+        echo -n " ."
+      done
+      echo ""
+    done
+  fi
+}
+
+timeTable () {
+  n=$1; i=1
+  while [ $i -ne 10 ]; do
+    i=$(expr $i + 1)
+    table=$(expr $i \* $n)
+    echo $table
+  done
+}
+
+health () {
+  date;
+  echo "uptime:"
+  uptime
+  echo "Currently connected:"
+  w
+  echo "--------------------"
+  echo "Last logins:"
+  last -a |head -3
+  echo "--------------------"
+  echo "Disk and memory usage:"
+  df -h | xargs | awk '{print "Free/total disk: " $11 " / " $9}'
+  free -m | xargs | awk '{print "Free/total memory: " $17 " / " $8 " MB"}'
+  echo "--------------------"
+  start_log=`head -1 /var/log/messages |cut -c 1-12`
+  oom=`grep -ci kill /var/log/messages`
+  echo -n "OOM errors since $start_log :" $oom
+  echo ""
+  echo "--------------------"
+  echo "Utilization and most expensive processes:"
+  top -b |head -3
+  echo
+  top -b |head -10 |tail -4
+  echo "--------------------"
+  echo "Open TCP ports:"
+  nmap -p- -T4 127.0.0.1
+  echo "--------------------"
+  echo "Current connections:"
+  ss -s
+  echo "--------------------"
+  echo "processes:"
+  ps auxf --width=200
+  echo "--------------------"
+  echo "vmstat:"
+  vmstat 1 5
+}
+
+encrypt () {
+  file=$1;
+  keybase encrypt -i $file -o $file.keybase butlerx
+  rm -rf $file
+}
+
+decrypt () {
+  file=$1;
+  keybase decrypt -i $file
+}
+
+gpgEncrypt () {
+  file=$1;
+  gpg -c $file
+  rm -rf $file
+}
+
+gpgDecrypt () {
+  file=$1;
+  gpg -d $file
+}
+
+evenOdd () {
+  num=$(expr $1 % 2)
+  if [ $num -eq 0 ]; then
+    echo "is a Even Number"
+  else
+    echo "is a Odd Number"
+  fi
+}
+
+simpleCalc () {
+  clear
+  sum=0
+  i="y"; n1=$1; n2=$2;
+  while [ $i = "y" ]; do
+    echo "1.Addition"
+    echo "2.Subtraction"
+    echo "3.Multiplication"
+    echo "4.Division"
+    echo "Enter your choice"
+    read ch
+    case $ch in
+      1)sum=`expr $n1 + $n2`
+        echo "Sum ="$sum;;
+      2)sum=`expr $n1 - $n2`
+        echo "Sub = "$sum;;
+      3)sum=`expr $n1 \* $n2`
+        echo "Mul = "$sum;;
+      4)sum=`expr $n1 / $n2`
+        echo "Div = "$sum;;
+      *)echo "Invalid choice";;
+    esac
+    echo "Do u want to continue (y/n)) ?"
+    read i
+  done
+}
+
+fibonacci () {
+  a=$1; fact=1
+  while [ $a -ne 1 ]; do
+    fact=$(expr $fact \* $a)
+    a=$(expr $a - 1)
+    echo $fact
+  done
+}
+
+fractorial () {
+  a=$1; fact=1
+  while [ $a -ne 0 ]; do
+    fact=$(expr $fact \* $a)
+    a=$(expr $a - 1)
+  done
+  echo $fact
 }
