42779499c90a675bb82e28cd0aca38758acbeff6
butlerx <cianbutlerx@gmail.com>
2016-12-21 09:27:55 +0000

    .

---
+++ zsh/chip.zsh
@@ -0,0 +1,8 @@
+alias sshon="sudo systemctl start ssh"
+alias sshoff="sudo systemctl stop ssh"
+alias getip="ip addr | awk '/inet/ {print }'"
+alias ll="ls -al"
+alias pgr="ps -ef | grep"
+alias apti="sudo apt install"
+alias aptu="sudo apt update"
+alias apts="apt-cache search"
