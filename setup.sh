5e3f7ae97098b6dbea313a20afc1288b6151e112
butlerx <cianbutlerx@gmail.com>
2016-12-22 18:34:46 +0000

    It worked for me...

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #vim
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
@@ -5,8 +6,9 @@ ln -s $HOME/.dotfiles/vimrc $XDG_CONFIG_HOME/nvim/init.vim
 nvim +qall
 #zsh
 ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
+git submodule init
+git submodule update
 #tmux
 ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 sudo pip install psutil
-sudo cp ~/.dotfiles/tmux/basic-cpu-and-memory.tmux /usr/local/bin/tmux-mem-cpu-load
 tmux source-file $HOME/.tmux.conf
