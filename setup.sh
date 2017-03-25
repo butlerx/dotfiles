057b564643250930d74fa80e89f2aa4424316496
butlerx <cianbutlerx@gmail.com>
2017-03-25 12:37:33 +0000

    ALL SORTS OF THINGS

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#vim
+
+git clone https://github.com/butlerx/dotfiles.git .dotfiles
+# vim
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 ln -s $HOME/.dotfiles/vimrc $XDG_CONFIG_HOME/nvim/init.vim
 nvim +qall
-#zsh
+# zsh
 ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
 git submodule init
 git submodule update
-#tmux
+# tmux
 ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
-sudo pip install psutil
+pip install --user psutil
 tmux source-file $HOME/.tmux.conf
+# weechat
+ln -s $HOME/.dotfiles/weechat $HOME/.weechat
