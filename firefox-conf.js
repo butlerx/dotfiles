450af22735be67e3c47e21b9c8255ae1fa2cb840
Cian Butler <butlerx@notthe.cloud>
2025-01-17 10:03:38 +0000

    Fucking templates.

--- firefox-conf.js
+++ firefox-conf.js
@@ -1,13 +1,11 @@
-/* user.js
- * https://github.com/rafaelmardojai/firefox-gnome-theme/
- */
+// pets: package=firefox
+// pets: symlink=~/.mozilla/firefox/butlerx.default/user.js
 
 // Enable customChrome.css
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true);
 
 // Enable CSD
-user_pref("browser.tabs.drawInTitlebar", true);
+user_pref('browser.tabs.drawInTitlebar', true);
 
 // Set UI density to normal
-user_pref("browser.uidensity", 0);
-
+user_pref('browser.uidensity', 0);
